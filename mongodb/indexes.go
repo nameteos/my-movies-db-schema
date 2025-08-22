d95c544bcac646f7dfc427b81b4f42a825762c71
nameteos <jeroen.soet@gmail.com>
2025-08-22 10:38:12 +0200

    remove atlas search

--- mongodb/indexes.go
+++ mongodb/indexes.go
@@ -13,17 +13,11 @@ type MongoIndexer struct {
 }
 
 func (r MongoIndexer) CreateIndexes(ctx context.Context) ([]string, error) {
-	models := vectorSearchIndexes(r.VectorDimensions)
-	vectorIndexes, err := r.Collection.SearchIndexes().CreateMany(ctx, models)
-	if err != nil {
-		return nil, err
-	}
 	indexes, err := r.Collection.Indexes().CreateMany(ctx, movieIndexes())
 	if err != nil {
 		return nil, err
 	}
-
-	return append(vectorIndexes, indexes...), nil
+	return indexes, nil
 }
 
 func movieIndexes() []mongo.IndexModel {
@@ -50,43 +44,4 @@ func movieIndexes() []mongo.IndexModel {
 	}
 
 	return []mongo.IndexModel{textIdx, createdAtIdx, yearIdx}
-}
-
-// VectorSearchIndexes returns Atlas Search index models that enable
-// nearest neighbour (k-NN) vector search on embeddings.
-//
-// This uses a wildcard mapping so any embedding stored under
-// embeddings.<name>.embedding will be searchable. Pass the vector
-// dimensionality of your embedding model (e.g., 1536 for many OpenAI text models).
-//
-// Note: These are Atlas Search indexes and must be created via the
-// SearchIndexes() helper on a collection (not with createIndexes).
-func vectorSearchIndexes(dimensions int) []mongo.SearchIndexModel {
-	definition := bson.D{
-		{Key: "mappings", Value: bson.D{
-			{Key: "dynamic", Value: false},
-			{Key: "fields", Value: bson.D{
-				{Key: "embeddings", Value: bson.D{
-					{Key: "fields", Value: bson.D{
-						{Key: "*", Value: bson.D{
-							{Key: "fields", Value: bson.D{
-								{Key: "embedding", Value: bson.D{
-									// As of Atlas Search 2024+, "vector" with numDimensions is preferred.
-									{Key: "type", Value: "vector"},
-									{Key: "numDimensions", Value: dimensions},
-									{Key: "similarity", Value: "cosine"},
-								}},
-							}},
-						}},
-					}},
-				}},
-			}},
-		}},
-	}
-
-	return []mongo.SearchIndexModel{
-		{
-			Definition: definition,
-		},
-	}
 }
